d1662518eb429a85c45dc26e289ab4d424096040
Ken Copas <82295445+kencopas@users.noreply.github.com>
2025-04-07 08:15:55 -0400

    image_generator.py

--- Cognition/image_generator.py
+++ Cognition/image_generator.py
@@ -1,17 +1,10 @@
 from random import randint, uniform
 
-
-# my_image = (0.4, 0.4, 0.4,
-#             0.4, 0, 0,
-#             0.2, 0.8, 0)
-
-
-
 def gen_set(n, length):
     my_dict = {}
-    weight_graph = [round(uniform(0, 1), 1) for _ in range(length**2)]
+    skew = uniform(0, 2)
+    weight_graph = [round(uniform(0, 1)**skew, 1) for _ in range(length**2)]
     for _ in range(n):
-        # new_image = my_image # Custom image
         new_image = tuple(randint(0,10)/10 for _ in range(length**2)) # Random image
         my_dict[new_image] = eval_image(new_image, weight_graph)
     return my_dict
